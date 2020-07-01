b64e3b0581576501ffc64690234902ba9c4de936
VelinovAngel <66844069+VelinovAngel@users.noreply.github.com>
2020-07-01 07:54:41 +0200

    Add files via upload

---
+++ 0.1.National_Court/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _0._1.National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstGroupPeopleForHours = int.Parse(Console.ReadLine());
+            int secondGroupPeopleForHours = int.Parse(Console.ReadLine());
+            int thirtGroupPeopleForHours = int.Parse(Console.ReadLine());
+
+            int peopleHelp = int.Parse(Console.ReadLine());
+
+            int totalGroupPeopleForHours = firstGroupPeopleForHours + secondGroupPeopleForHours + thirtGroupPeopleForHours;
+
+            int timeNeed = 0;
+
+            while (peopleHelp > 0)
+            {
+                timeNeed++;
+                if (timeNeed % 4 == 0)
+                {
+                    continue;
+                }
+                peopleHelp -= totalGroupPeopleForHours; 
+            }
+            Console.WriteLine($"Time needed: {timeNeed}h.");
+        }
+    }
+}
