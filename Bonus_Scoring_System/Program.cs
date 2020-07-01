63bd3946b1f9ca6bf3ff9032480571c452f2ed6c
VelinovAngel <66844069+VelinovAngel@users.noreply.github.com>
2020-07-01 07:59:45 +0200

    Add files via upload

---
+++ Bonus_Scoring_System/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+
+namespace Bonus_Scoring_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	On the first line you are going to receive the count of the students – an integer number in the range[0…50]
+            //•	On the second line you are going to receive the count of the lectures – an integer number in the range[0...50].
+            //•	On the third line you are going to receive the initial bonus – an integer number in the range[0….100].
+            //•	On the next lines, you will be receiving the attendances of each student.
+            //•	There will never be students with equal bonuses.
+
+            int numberOfStudent = int.Parse(Console.ReadLine());
+            int lectures = int.Parse(Console.ReadLine());
+            int additionalBonus = int.Parse(Console.ReadLine());
+
+            //{total bonus} = {student attendances} / {course lectures} * (5 + {additional bonus})
+
+            double maxAttendence = 0;
+            int maxLectures = 0;
+            double result = 0;
+
+            for (int i = 1; i <= numberOfStudent; i++)
+            {
+                int attendancesStudent = int.Parse(Console.ReadLine());
+
+                if (attendancesStudent > maxAttendence)
+                {
+                    maxAttendence = attendancesStudent;
+                    maxLectures = attendancesStudent;
+                }
+                if (lectures > 0)
+                {
+
+                    result = (1.0 * maxAttendence / lectures) * (5 + additionalBonus);
+                }
+            }
+            //maxAttendence *= 1000;
+            //maxAttendence /= lectures;
+            //double result = (double)maxAttendence;
+            //result /= 1000;
+            Console.WriteLine($"Max Bonus: {Math.Ceiling(result)}.");
+            Console.WriteLine($"The student has attended {maxLectures} lectures.");
+        }
+    }
+}
---
+++ Inventory/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+
+namespace Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lists = Console
+                .ReadLine()
+                .Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            //•	"Collect - {item}" – Receiving this command, you should add the given item in your inventory. If the item already exists, you should skip this line.
+            //•	"Drop - {item}" – You should remove the item from your inventory, if it exists.
+            //•	"Combine Items - {oldItem}:{newItem}" – You should check if the old item exists, if so, add the new item after the old one. Otherwise, ignore the command.
+            //•	"Renew – {item}" – If the given item exists, you should change its position and put it last in your inventory.
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "Craft!")
+            {
+                string[] splittedCommand = command.Split(new char[] { '-', ' ' }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+                if (splittedCommand[0] == "Collect")
+                {
+                    if (!lists.Contains(splittedCommand[1]))
+                    {
+                        lists.Add(splittedCommand[1]);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else if (splittedCommand[0] == "Drop")
+                {
+                    if (lists.Contains(splittedCommand[1]))
+                    {
+                        lists.Remove(splittedCommand[1]);
+                    }
+                }
+                else if (splittedCommand[0] == "Combine" && splittedCommand[1] == "Items")
+                {
+                    string[] items = splittedCommand[2].Split(new char[] { ':', ' ' }
+               , StringSplitOptions.RemoveEmptyEntries);
+                    string oldItems = items[0];
+                    string newItems = items[1];
+
+                    if (lists.Contains(items[0]))
+                    {
+
+                        lists.Insert(lists.IndexOf(oldItems) + 1, newItems);
+                    }
+
+                }
+                else if (splittedCommand[0] == "Renew")
+                {
+                    if (lists.Contains(splittedCommand[1]))
+                    {
+
+                        lists.RemoveAt(lists.IndexOf(splittedCommand[1]));
+                        lists.Add(splittedCommand[1]);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", lists));
+        }
+    }
+}
