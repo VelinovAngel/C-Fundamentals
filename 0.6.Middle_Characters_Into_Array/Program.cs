e8c155e5e2f79227de2dc54adc6e801d30d8441c
VelinovAngel <66844069+VelinovAngel@users.noreply.github.com>
2020-07-01 07:55:50 +0200

    Add files via upload

---
+++ 0.6.Middle_Characters_Into_Array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _0._6.Middle_Characters_Into_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+
+            PrintMiddleChar(array);
+        }
+
+        static void PrintMiddleChar(string array)
+        {
+            if (array.Length % 2 != 0)
+            {
+                for (int i = array.Length / 2; i < (array.Length / 2) + 1; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            else 
+            {
+                for (int i = (array.Length / 2) - 1; i < (array.Length / 2) + 1; i++)
+                {
+                    Console.Write(array[i]);
+                }
+            }
+        }
+    }
+}
